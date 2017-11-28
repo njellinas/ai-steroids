8b11cba7aa55de3efaeb6960ee854a47e7dc4836
nickbrick <nickbrick@hotmail.com>
2017-11-28 21:11:00 +0200

    add cart_Q.py, add QLearn.py

--- cartpole_v1/Q_learning/QLearn.py
+++ cartpole_v1/Q_learning/QLearn.py
@@ -1,6 +1,28 @@
+from bisect import bisect
+
+
 class QLearn:
-    def quantize(min=0.0, max=1.0, buckets=5):
+    def quantized(min=0.0, max=1.0, buckets=5):
         if min >= max:
             raise ValueError
         else:
-            return [k * (max - min) / buckets for k in range(buckets)]
+            return [min + k * (max - min) / buckets for k in range(buckets)]
+
+    def matchBucket(value, buckets):
+        if value < buckets[0]:
+            return None
+        elif value > buckets[1] - buckets[0] + buckets[-1]:
+            return None
+        else:
+            idxes = list(range(len(buckets)))
+            i = bisect(buckets, value) - 1
+            return idxes[i]
+
+
+if __name__ == '__main__':
+    min, max, num_buckets = -5.0, 10.0, 5
+    my_buckets = QLearn.quantized(min, max, num_buckets)
+    print('min: {}, max: {}, quantized as {} buckets: {}'.format(min, max, num_buckets, my_buckets))
+    example_vals = [-5.0, 3.9999, 10.0, 11.0, -5.1]
+    for value in example_vals:
+        print('value: {} falls in buckets[{}]'.format(value, QLearn.matchBucket(value, my_buckets)))
--- cartpole_v1/Q_learning/cart_Q.py
+++ cartpole_v1/Q_learning/cart_Q.py
@@ -1,5 +1,6 @@
 import gym
 import numpy
+from QLearn import *
 '''
 inputs: [x, x', theta, theta']
 output: 1 = right, 0 = left
@@ -10,20 +11,25 @@ env = gym.make('CartPole-v1')
 env._max_episode_steps = 500
 
 RENDER = False
+bucketed_observations = list()
+div = 10
+observation_space_dimensionality = env.observation_space.shape[0]
+for i_dimension in range(observation_space_dimensionality):
+    bucketed_observations.append(QLearn.quantized(-5, 5, div))
 
-
+Q = numpy.zeros((div, div, div, div, env.action_space.n))
 observation = env.reset()
+action = numpy.random.random_integers(0, 1)
+observation_as_buckets = [QLearn.matchBucket(observation[i], bucketed_observations[i]) for i in range(observation_space_dimensionality)]
+stateaction = tuple(observation_as_buckets) + (action,)
+Q[stateaction] = reward + gamma * max( next_state_all_actions_future_Q )
 
-for t in range(env._max_episode_steps):
+
+#for t in range(env._max_episode_steps):
+while not done:
     if RENDER:
         env.render()
-    x = numpy.array(observation)
-    logp = numpy.dot(W, x)
-    p = 1.0 / (1.0 + numpy.exp(-logp)) # sigmoid function (gives probability of going up)
-    if p > 0.5:
-        action = 1
-    else:
-        action = 0
+
     observation, reward, done, info = env.step(action)
     if done:
         if RENDER:
