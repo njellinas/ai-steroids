1a8bffc77abce05a0e224a3bf9e23bdc7c66609e
nickbrick <nickbrick@hotmail.com>
2017-11-29 03:04:32 +0200

    den3erw

--- cartpole_v1/Q_learning/cart_Q.py
+++ cartpole_v1/Q_learning/cart_Q.py
@@ -1,6 +1,7 @@
 import gym
-import numpy
-from QLearn import *
+import numpy as np
+from QLearn import QLearn
+import pickle
 '''
 inputs: [x, x', theta, theta']
 output: 1 = right, 0 = left
@@ -8,30 +9,64 @@ output: 1 = right, 0 = left
 
 
 env = gym.make('CartPole-v1')
-env._max_episode_steps = 500
+MAX_T = 250
+NUM_EPISODES = 100000
+alpha = 0.5
+gamma = 0.9
+env._max_episode_steps = MAX_T
+LOAD_PRETRAINED = True
+
+TRAIN = False
 
 RENDER = False
-bucketed_observations = list()
-div = 10
+bucketed_states = list()
+num_buckets = [2, 2, 14, 6]
+mins = [-10, -10, -0.5, -0.5]
+maxes = [10, 10, 0.5, 0.5]
 observation_space_dimensionality = env.observation_space.shape[0]
 for i_dimension in range(observation_space_dimensionality):
-    bucketed_observations.append(QLearn.quantized(-5, 5, div))
+    bucketed_states.append(QLearn.quantized(mins[i_dimension], maxes[i_dimension], num_buckets[i_dimension]))
 
-Q = numpy.zeros((div, div, div, div, env.action_space.n))
-observation = env.reset()
-action = numpy.random.random_integers(0, 1)
-observation_as_buckets = [QLearn.matchBucket(observation[i], bucketed_observations[i]) for i in range(observation_space_dimensionality)]
-stateaction = tuple(observation_as_buckets) + (action,)
-Q[stateaction] = reward + gamma * max( next_state_all_actions_future_Q )
+# Q = np.zeros((div, div, div, div, env.action_space.n))
+Q = np.zeros(tuple(num_buckets) + (env.action_space.n,))
+if LOAD_PRETRAINED:
+    try:
+        with open('Q.pkl', 'rb') as f:
+            Q = pickle.load(f)
+    except FileNotFoundError:
+        pass
 
+if TRAIN:
+    for i_episode in range(NUM_EPISODES):
+        observation = env.reset()
 
-#for t in range(env._max_episode_steps):
-while not done:
-    if RENDER:
+        for t in range(MAX_T):
+            if RENDER:
+                env.render()
+            action = np.random.randint(env.action_space.n)  # *
+            observation, reward, done, info = env.step(action)
+
+            state_as_buckets = tuple([QLearn.matchBucket(observation[i], bucketed_states[i]) for i in range(observation_space_dimensionality)])
+            stateaction = state_as_buckets + (action,)
+            Q[stateaction] = (1 - alpha) * Q[stateaction] + alpha * (reward + gamma * np.amax(Q[state_as_buckets]))
+            if done:
+                if RENDER:
+                    print("Episode {} finished after {} timesteps".format(i_episode, t+1))
+                break
+
+    with open('Q.pkl', 'wb') as f:
+        pickle.dump(Q, f)
+
+else:
+    observation = env.reset()
+    for t in range(MAX_T):
         env.render()
-
-    observation, reward, done, info = env.step(action)
-    if done:
-        if RENDER:
-            print("Episode {} finished after {} timesteps".format(i_episode, t+1))
-        break
+        state_as_buckets = tuple([QLearn.matchBucket(observation[i], bucketed_states[i]) for i in range(observation_space_dimensionality)])
+        pair = Q[state_as_buckets]
+        action = np.argmax(Q[state_as_buckets])
+        
+        print(action)
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
