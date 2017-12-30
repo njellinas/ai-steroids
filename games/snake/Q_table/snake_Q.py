08f57e05491bc43d41eae51a9c71e7d52431688b
nickbrick <nickbrick@hotmail.com>
2017-12-30 21:11:30 +0200

    change location of pickle, refactor snake_Q.py

--- games/snake/Q_table/snake_Q.py
+++ games/snake/Q_table/snake_Q.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import pickle
 
-from QLearn import QLearn
+# from QLearn import QLearn
 
 
 env = gym.make('Snake-v0')
@@ -11,9 +11,9 @@ env = gym.make('Snake-v0')
 MAX_T = 40000
 env._max_episode_steps = MAX_T
 
-NUM_EPISODES = 10000
+NUM_EPISODES = 100000
 
-gamma = 0.99
+gamma = 0.1
 
 MIN_DELTA = 0.01
 def delta(t):
@@ -29,27 +29,38 @@ def select_action(state, delta):
         action = env.action_space.sample()
     # Select the action with the highest q
     else:
-        action = np.argmax(Q[state])
+        Q_state = np.array([Q_of(state, action) for action in range(env.action_space.n)])
+        action = np.argmax(Q_state)
     return action
 
 
-LOG = True * False
 def log_message(s):
     global LOG
     if LOG:
         print(s)
 
+LOAD_PRETRAINED = True
 TRAIN = True #* False #**************************************************************************
-LOAD_PRETRAINED = False
+LOG = True * False
+RENDER = True * False
 
-RENDER = False
 
 
-Q = np.zeros(tuple(env.observation_space.n) + (env.action_space.n,))
+state_space_dimensionality = 3 # env.observation_space.shape[0]
+# Q = np.zeros((220,2**10,220,4),dtype=np.int8)
+Q = dict()
+def Q_of(state, action):
+    global Q
+    head, shape, food = state
+    key = ','.join([str(head), str(shape), str(food), str(action)])
+    if key in Q.keys():
+        return Q[key]
+    else:
+        return 0
 
 if LOAD_PRETRAINED:
     try:
-        with open('Q_table.pkl', 'rb') as f:
+        with open(r'games\snake\Q_table\snake_Q_table.pkl', 'rb') as f:
             Q = pickle.load(f)
     except FileNotFoundError:
         pass
@@ -63,41 +74,47 @@ if TRAIN:
                 score = scores / 100
                 print('100 ep mean score: {}'.format(score))
                 scores = 0
-        observation = env.reset()
-        state_as_buckets = tuple([QLearn.matchBucket(observation[i], bucketed_states[i]) for i in range(observation_space_dimensionality)])
+                if i_episode % 10000 == 0:
+                    with open(r'games\snake\Q_table\snake_Q_table.pkl', 'wb') as f:
+                        pickle.dump(Q, f)
+        state = env.reset()
+        rewards = 0
         for t in range(MAX_T):
             if RENDER:
                 env.render()
-            action = select_action(state_as_buckets, delta(t))
+            action = select_action(state, delta(t))
 
-            log_message('state={}'.format(observation))
-            log_message('state buckets={}'.format(state_as_buckets))
+            log_message('state={}'.format(state))
             log_message('action={}'.format(action))
-            log_message('Q[{}]={}'.format(state_as_buckets, Q[state_as_buckets]))
 
-            observation, reward, done, info = env.step(action)
-            next_state_as_buckets = tuple([QLearn.matchBucket(observation[i], bucketed_states[i]) for i in range(observation_space_dimensionality)])
-            stateaction = state_as_buckets + (action,)
-            Q[stateaction] = (1 - alpha(t)) * Q[stateaction] + alpha(t) * (reward + gamma * np.amax(Q[next_state_as_buckets]))
-            log_message('Q[{}]={}'.format(stateaction, Q[stateaction]))
-            state_as_buckets = next_state_as_buckets
+            state, reward, done, info = env.step(action)
+            rewards += reward
+            next_state = tuple([state[i] for i in range(state_space_dimensionality)])
+            # stateaction = state + (action,)
+            stateaction = ','.join([str(x) for x in state]+[str(action)])
+            Q_next_state = [Q_of(next_state, action) for action in range(env.action_space.n)]
+            Q[stateaction] = (1 - alpha(t)) * Q_of(state, action) + alpha(t) * (reward + gamma * np.amax(Q_next_state))
+            log_message('Q[{}]={}'.format(stateaction, Q_of(state, action)))
+            state = next_state
             if done:
-                log_message("Episode {} finished after {} timesteps".format(i_episode, t + 1))
-                scores += t + 1
+                log_message("Episode {} finished after {} timesteps, total reward {}".format(i_episode, t + 1, rewards))
+                scores += rewards
                 break
 
-    with open('Q_table.pkl', 'wb') as f:
+    with open(r'games\snake\Q_table\snake_Q_table.pkl', 'wb') as f:
         pickle.dump(Q, f)
 
 else:
     for _ in range(NUM_EPISODES):
-        observation = env.reset()
+        state = env.reset()
         for t in range(MAX_T):
             env.render()
-            state_as_buckets = tuple([QLearn.matchBucket(observation[i], bucketed_states[i]) for i in range(observation_space_dimensionality)])
-            pair = Q[state_as_buckets]
-            action = np.argmax(Q[state_as_buckets])
-            observation, reward, done, info = env.step(action)
+            Q_state = [Q_of(state, action) for action in range(env.action_space.n)]
+            action = np.argmax(Q_state)
+            action_Q = np.amax(Q_state)
+            print(action)
+            print(action_Q)
+            state, reward, done, info = env.step(action)
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 break
